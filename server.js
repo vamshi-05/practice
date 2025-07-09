be6f35080827caeacd008526e72b55ea0666ac29
Shanigala Vamshi <shanigalavamshi@gmail.com>
2025-07-09 22:49:11 +0530

    commit1 in demo1

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+const sampleRouter = require('./route');S
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!!');
 });
+
+app.use('/sample', sampleRouter);
 
 app.get('/about', (req, res) => {
     res.send('About page');
