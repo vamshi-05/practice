865687cf8f2adfc9b574796ba986f24d90915aa3
Shanigala Vamshi <shanigalavamshi@gmail.com>
2025-07-09 22:58:43 +0530

    update in main

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-const sampleRouter = require('./route');S
+const sampleRouter = require('./route');
 
 
 app.get('/', (req, res) => {
